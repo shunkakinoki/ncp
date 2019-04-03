9b70a987b503a6f28bbbb02aba6d27ab200d9ff1
Shun Kakinoki <shunkakinoki@gmail.com>
2019-04-03 12:35:50 +0800

    finish study for today

---
+++ beginner/121/answer_4.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+long A, B;
+
+long res(long in)
+{
+    long ans = 0;
+    if (in == 1)
+        return 1;
+    if (in % 2 == 0)
+    {
+        if ((in / 2) % 2 == 0)
+        {
+            ans = in ^ 0;
+        }
+        else
+        {
+            ans = in ^ 1;
+        }
+    }
+    else
+    {
+        if (((in + 1) / 2) % 2 == 0)
+        {
+            ans = 0;
+        }
+        else
+        {
+            ans = 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    cin >> A >> B;
+    //cout<<res(A-1)<<endl;
+    //cout<<res(B)<<endl;
+    if (A == 0)
+    {
+        cout << res(B) << endl;
+    }
+    else
+    {
+        cout << (res(A - 1) ^ res(B)) << endl;
+    }
+}
