fe2be2e7b884f967a7ccd245c97c177b696e070a
Shun Kakinoki <shunkakinoki@gmail.com>
2019-04-04 13:26:03 +0800

    added 122 answer_4.cpp

--- beginner/122/answer_4.cpp
+++ beginner/122/answer_4.cpp
@@ -1,44 +1,58 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include <algorithm>
 #include <vector>
 using namespace std;
 
-int dp[101][4][4][4];
+const int M = 1e9 + 7;
+long long dp[101][4][4][4];
 
 int main()
 {
     int N;
     cin >> N;
 
-    dp[0][3][3][3] = 1;
-
+    dp[0][0][0][0] = 1;
     for (int len = 0; len < N; len++)
     {
-        for (int c1 = 0; c1 < 4; c1++)
+        for (int i = 0; i < 4; i++)
         {
-            for (int c2 = 0; c2 < 4; c2++)
+            for (int j = 0; j < 4; j++)
             {
-                for (int c3 = 0; c3 < 4; c3++)
+                for (int k = 0; k < 4; k++)
                 {
-                    if (dp[len][c1][c2][c3] == 0)
-                        continue;
-                    for (int a = 0; a < 4; a++)
+                    for (int x = 0; x < 4; x++)
                     {
-                        if (a == 2 && c1 == 1 && c2 == 0)
+                        if (x == 0 && j == 1 && k == 2)
                             continue;
-                        if (a == 2 && c1 == 1 && c2 == 0)
+                        if (x == 0 && i == 1 && k == 2)
                             continue;
-                        if (a == 2 && c1 == 1 && c2 == 0)
+                        if (x == 0 && i == 1 && j == 2)
                             continue;
-                        if (a == 2 && c1 == 1 && c2 == 0)
+                        if (x == 1 && i == 0 && j == 2)
                             continue;
-                        if (a == 2 && c1 == 1 && c2 == 0)
+                        if (x == 0 && i == 2 && j == 1)
                             continue;
+
+                        dp[len + 1][x][i][j] =
+                            (dp[len + 1][x][i][j] + dp[len][i][j][k]) % M;
                     }
                 }
             }
         }
     }
+
+    long long ans = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            for (int k = 0; k < 4; k++)
+            {
+                ans = (ans + dp[N][i][j][k]) % M;
+            }
+        }
+    }
+
+    cout << ans << endl;
 }
