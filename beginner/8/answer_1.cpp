2feb13bb5772642d1134cfd0b76d06ee2a060c6e
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-09 21:24:01 +0900

    finish 8

---
+++ beginner/8/answer_1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string S;
+    cin >> S;
+    if (S == "a") cout << -1 << endl;
+    else cout << 'a' << endl;
+
+    return 0;
+}
