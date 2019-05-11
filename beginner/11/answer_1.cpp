049cfeaa590d579b6db6d077043a5b17143a4396
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-12 08:31:08 +0900

    init 11 abc

---
+++ beginner/11/answer_1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <map>
+
+#define cinf(n, x)                \
+    for (int i = 0; i < (n); i++) \
+        cin >> x[i];
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    cinf(n, a);
+}
