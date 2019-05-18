65507b4e32ff923efec07945636d8cc4e6664014
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-18 21:14:21 +0900

    finish abc7

--- beginner/7/answer_1.cpp
+++ beginner/7/answer_1.cpp
@@ -7,11 +7,7 @@ using namespace std;
 
 int main()
 {
-    int N;
-    cin >> N;
-    if (N % 3 == 0)
-        cout << "YES" << endl;
-    else
-        cout << "NO" << endl;
-    ;
+    int n;
+    cin >> n;
+    cout << n - 1 << endl;
 }
--- beginner/7/answer_2.cpp
+++ beginner/7/answer_2.cpp
@@ -7,21 +7,12 @@ using namespace std;
 
 int main()
 {
-    int A, B, K;
-    cin >> A >> B >> K;
-    int ans = 0, c = 0;
-    for (int i = 100; i > 0; i--)
-    {
-        if (A % i == 0 && B % i == 0)
-        {
-            c++;
-            if (c >= K)
-            {
-                ans = i;
-                break;
-            }
-        }
-    }
-    cout << ans;
+    string S;
+    cin >> S;
+    if (S == "a")
+        cout << -1 << endl;
+    else
+        cout << 'a' << endl;
+
     return 0;
 }
