3fd4ad8e890f18e76c687408a33e494af5420307
Shun Kakinoki <shunkakinoki@gmail.com>
2019-05-18 21:15:48 +0900

    comp abc8

--- beginner/8/answer_1.cpp
+++ beginner/8/answer_1.cpp
@@ -5,11 +5,9 @@
 #include <algorithm>
 using namespace std;
 
-int main() {
-    string S;
-    cin >> S;
-    if (S == "a") cout << -1 << endl;
-    else cout << 'a' << endl;
-
-    return 0;
+int main()
+{
+    int s, t;
+    cin >> s >> t;
+    cout << t - s + 1 << endl;
 }
--- beginner/8/answer_2.cpp
+++ beginner/8/answer_2.cpp
@@ -3,16 +3,30 @@
 #include <cmath>
 #include <vector>
 #include <algorithm>
+#include <map>
+
 using namespace std;
 
-int main()
+int n;
+string s[55];
+map<string, int> mp;
+
+int main(void)
 {
-    string S;
-    cin >> S;
-    if (S == "a")
-        cout << -1 << endl;
-    else
-        cout << 'a' << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++)
+        cin >> s[i];
+    for (int i = 1; i <= n; i++)
+        mp[s[i]]++;
+
+    int mx = 0;
+    string mx_s;
+    for (auto x : mp)
+    {
+        if (x.second > mx)
+            mx = x.second, mx_s = x.first;
+    }
+    cout << mx_s << endl;
 
     return 0;
 }
